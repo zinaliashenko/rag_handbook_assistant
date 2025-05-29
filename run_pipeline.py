0194bbd8ae9b7d64638f22f9d6a986fc984bfcd2
zinaliashenko <liashenkozina@gmail.com>
2025-05-29 13:06:09 +0100

    autoformat code

--- run_pipeline.py
+++ run_pipeline.py
@@ -12,13 +12,12 @@ $ python run_pipeline.py
 
 from utils.pipeline import run_data_pipeline, run_query_answer_pipeline
 
-
 if __name__ == "__main__":
 
     print("[CLI MODE] Start pipeline...")
     run_data_pipeline()
 
-    #user_query = input("Input your query: ")
-    #run_query_answer_pipeline(query=user_query, 
-                              #run_mode="console",
-                              #llm_provider="groq")
+    user_query = input("Input your query: ")
+    run_query_answer_pipeline(query=user_query,
+                              run_mode="console",
+                              llm_provider="groq")
